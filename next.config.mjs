76f76e406680d321172f210aaa24d4c471675397
ivan <ivanlee1999@163.com>
2026-03-21 19:18:52 +0000

    feat: add Dockerfile + docker-compose for self-hosting

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false },
+  output: "standalone",
 };
 
 export default nextConfig;
